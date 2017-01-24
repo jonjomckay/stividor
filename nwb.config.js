126a9109bf39fc466a0237e7956d1ebb67bf378c
Jonjo McKay <jonjo@jonjomckay.com>
2017-01-24 13:38:42 +0000

    Migrated to nwb for better ES6 support when building

---
+++ nwb.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+
+require('dotenv').config();
+
+var REACT_APP = /^REACT_APP_/i;
+
+function getEnvironment() {
+    const processEnv = Object.keys(process.env)
+        .filter(key => REACT_APP.test(key))
+        .reduce((env, key) => {
+            env[key] = JSON.stringify(process.env[key]);
+            return env;
+        }, {
+            // Useful for determining whether we’re running in production mode.
+            // Most importantly, it switches React into the correct mode.
+            'NODE_ENV': JSON.stringify(
+                process.env.NODE_ENV || 'development'
+            )
+        });
+
+    return {'process.env': processEnv};
+}
+
+module.exports = {
+    type: 'react-app',
+    babel: {
+        cherryPick: ['semantic-ui-react', 'kubernetes-client', 'octokat']
+    },
+    webpack: {
+        uglify: false,
+        extra: {
+            node: {
+                fs: 'empty',
+                net: 'empty',
+                tls: 'empty'
+            },
+            plugins: [
+                new webpack.IgnorePlugin(/vertx/),
+                new webpack.DefinePlugin(getEnvironment()),
+            ]
+        }
+    }
+};
