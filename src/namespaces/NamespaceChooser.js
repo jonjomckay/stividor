a831085857fac31e9b2b40db0d4c2f97a5772504
Jonjo McKay <jonjo@jonjomckay.com>
2017-03-05 19:10:52 +0000

    Made the namespace chooser loadable

--- src/namespaces/NamespaceChooser.js
+++ src/namespaces/NamespaceChooser.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Select } from "semantic-ui-react";
-import Kubernetes from "../clients/Kubernetes";
+import Kubernetes from '../clients/Kubernetes';
+import Loadable from '../common/Loadable';
 
 class NamespaceChooser extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            loading: true,
             namespaces: []
         }
     }
@@ -21,13 +23,15 @@ class NamespaceChooser extends Component {
 
     componentDidMount = () => {
         // If a list of namespaces was passed in via props then use that, otherwise fetch the list from Kubernetes
-        if (this.props.namespaces) {
+        if (this.props.namespaces && this.props.namespaces.length) {
             this.setState({
+                loading: false,
                 namespaces: this.props.namespaces
             });
         } else {
             Kubernetes.fetchNamespaces((err, response) => {
                 this.setState({
+                    loading: false,
                     namespaces: response.items.map(item => item.metadata.name)
                 })
             });
@@ -43,23 +47,28 @@ class NamespaceChooser extends Component {
         });
 
         let value;
-        if (options.length) {
+        if (this.props.selectedValue) {
+            value = this.props.selectedValue;
+        } else if (options.length) {
             value = options[0].value;
         }
 
         return (
-            <Select fluid
-                    defaultValue={ value }
-                    onChange={ this.props.onChange }
-                    options={ options }
-                    placeholder="Choose a namespace" />
+            <Loadable loading={ this.state.loading }>
+                <Select fluid
+                        defaultValue={ value }
+                        onChange={ this.props.onChange }
+                        options={ options }
+                        placeholder="Choose a namespace" />
+            </Loadable>
         );
     }
 }
 
 NamespaceChooser.propTypes = {
     namespaces: React.PropTypes.arrayOf(React.PropTypes.string),
-    onChange: React.PropTypes.func.isRequired
+    onChange: React.PropTypes.func.isRequired,
+    selectedValue: React.PropTypes.string
 };
 
 export default NamespaceChooser;
